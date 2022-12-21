64c8e941a304b0b42046206eec5f3a3390a8abcb
daniel <persensus@yahoo.com.ar>
2022-12-21 19:20:33 -0300

    resueltos hasta el 04

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
+    let repeatStr = ""; 
+
+    if (num < 0){
+        return "ERROR"
+    }
+
+    for(let i=0; i < num; i++){
+        repeatStr += str
+    }
+    return repeatStr
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...arg) {
 
+    const arrFilter = arr.filter(item => {
+        for (const e of arg) {
+            if (item === e) {
+                return false
+            }
+        }
+        return true
+    })
+    return arrFilter
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;

27c403179507af0488b7343b3b02c75c28130341
daniel gerez <danielgerez999@hotmail.com>
2022-12-23 11:02:31 -0300

    resuelto hasta el ejercicio 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let menor = num1;
+    let mayor = num2
+    
+    if (num1 < 0 || num2 < 0 ||
+        typeof(num1) != "number" ||
+        typeof(num2) != "number" 
+        ){
+        return "ERROR"
+    }
 
+    if(num2 < num1){
+        menor = num2;
+        mayor = num1
+    }
+
+    console.log(menor, mayor)
+    for (let i = menor ; i <= mayor ;i++) {
+        sum += i    
+    }
+
+    return sum
 };
+
+console.log(sumAll(6, 4))
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(faren) {
+  let ToCelsius = (faren-32)*5/9;
+  return Math.round(ToCelsius * 10)/10
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(celsius) {
+let toFahren = celsius * 9/5 + 32
+  return Math.round(toFahren * 10)/10
 
+
+};
+console.log(convertToCelsius(100))
+console.log(convertToFahrenheit(37.8))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,

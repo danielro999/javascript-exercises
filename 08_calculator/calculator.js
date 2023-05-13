7596653f2fa54c9ef1d6a99720a8e6fc69b477e0
daniel <persensus@yahoo.com.ar>
2023-05-13 13:09:24 -0300

    ejercicio calculator termindo

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce((total, number) => {return total + number},0)
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((total, number) => total * number)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let contador = a 
+	if (a == 0) return 1
+  else {
+    for (let i = 1 ; i < a; i++ ){
+     contador *= i     
+    }
+  }
+  return contador
 };
+console.log(factorial(5))
 
 // Do not edit below this line
 module.exports = {

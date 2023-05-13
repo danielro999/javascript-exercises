45fd947c7186a8c7cc32e40ae846204e7bc2f5d2
daniel <persensus@yahoo.com.ar>
2023-05-13 17:34:19 -0300

    Exercise 11 - Get the Titles! ok!

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,21 +1,7 @@
-const books = [
-    {
-      title: 'Book',
-      author: 'Name'
-    },
-    {
-      title: 'Book2',
-      author: 'Name2'
-    }
-  ]
 
 const getTheTitles = function(books) {
-const title = books.map(libro => libro.title)
-return title
+return books.map(libro => libro.title)
 };
-
-
-console.log(getTheTitles(books))//['Book','Book2']
 
 // Do not edit below this line
 module.exports = getTheTitles;

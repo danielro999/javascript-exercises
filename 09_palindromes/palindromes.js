c3c18eb3c1d1767ba24f20e5d97b7abd4c75dbf8
daniel <persensus@yahoo.com.ar>
2023-05-13 15:31:26 -0300

    Exercise 09 - Palindromes ok!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const ArrayString= string
+                        .toLowerCase()
+                        .split("")
+                        .filter(letra => /^[A-Za-z]+$/.test(letra))
 
+    for (let index = 0; index < ArrayString.length; index++) {
+        const letra = ArrayString[index];
+        const letraInv = ArrayString[ArrayString.length - index - 1]
+        if (letra != letraInv)return false 
+    }
+    return true
 };
 
 // Do not edit below this line

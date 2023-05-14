17f359f907a53e561623e26fd5a9f724ade3faf3
daniel <persensus@yahoo.com.ar>
2023-05-14 13:26:57 -0300

    Exercise 12 - Find the Oldest OK!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((mayor, person)=>{
+      if (!mayor.yearOfDeath){
+        mayor.yearOfDeath = new Date().getFullYear() 
+      }
+        return mayor.yearOfDeath - mayor.yearOfBirth > person.yearOfDeath - person.yearOfBirth ? mayor : person
+    })
 
 };
+
+console.log(findTheOldest(people).name)
 
 // Do not edit below this line
 module.exports = findTheOldest;
